59b26d00cc1fefd0575e924cb1c24c759b25dd48
iuoon <1272461442@qq.com>
2020-11-26 23:22:13 +0800

    识别文字

--- BatchOcr.py
+++ BatchOcr.py
@@ -9,10 +9,13 @@ import tools.infer.utility as utility
 import tools.infer.predict_system as ocr_sys
 import time
 from pathlib import Path
+import configparser
+
 # from PIL import Image
 
 APP_TITLE = u'识别改名'
 APP_ICON = 'res/python.ico'
+
 
 # '道路识别'
 
@@ -24,14 +27,16 @@ class MainFrame(wx.Frame):
         self.SetSize((400, 160))
         self.Center()
 
-        self.pathLabel = wx.StaticText(self, -1, u'文件路径：', pos=(10, 20), size=(60, -1), name='pathLabel', style=wx.TE_LEFT)
+        self.pathLabel = wx.StaticText(self, -1, u'文件路径：', pos=(10, 20), size=(60, -1), name='pathLabel',
+                                       style=wx.TE_LEFT)
         self.path = wx.StaticText(self, -1, u'未选择', pos=(80, 20), size=(200, -1), name='path', style=wx.TE_LEFT)
 
         self.selectBtn = wx.Button(self, -1, u'选择', pos=(10, 50), size=(60, -1), style=wx.ALIGN_LEFT)
         self.startBtn = wx.Button(self, -1, u'开始', pos=(100, 50), size=(60, -1), style=wx.ALIGN_LEFT)
 
         self.tipLabel = wx.StaticText(self, -1, u'', pos=(10, 90), size=(200, -1), name='tipLabel', style=wx.TE_LEFT)
-        self.tipCurtLabel = wx.StaticText(self, -1, u'', pos=(80, 90), size=(200, -1), name='tipCurtLabel', style=wx.TE_LEFT)
+        self.tipCurtLabel = wx.StaticText(self, -1, u'', pos=(80, 90), size=(200, -1), name='tipCurtLabel',
+                                          style=wx.TE_LEFT)
 
         self.Bind(wx.EVT_BUTTON, self.OnSelect, self.selectBtn)
         self.Bind(wx.EVT_BUTTON, self.OnStart, self.startBtn)
@@ -41,12 +46,12 @@ class MainFrame(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             print(dlg.GetPath())  # 文件夹路径
             self.path.SetLabelText(dlg.GetPath())
-            self.tipLabel.SetLabelText("总共:"+str(len(os.listdir(self.path.GetLabelText()))))
+            self.tipLabel.SetLabelText("总共:" + str(len(os.listdir(self.path.GetLabelText()))))
         dlg.Destroy()
 
     def OnStart(self, event):
         if self.path.GetLabelText() == "未选择":
-            msgDialog = wx.MessageDialog(parent=None, message = u"选择文件夹",style = wx.OK)
+            msgDialog = wx.MessageDialog(parent=None, message=u"选择文件夹", style=wx.OK)
             if msgDialog.ShowModal() == wx.ID_OK:
                 print(1)
             return
@@ -59,28 +64,31 @@ class MainFrame(wx.Frame):
 
     def OnStart2(self):
         print(self.path.GetLabelText())
-        #fileNames = os.listdir(self.path.GetLabelText())
+        # fileNames = os.listdir(self.path.GetLabelText())
 
-        #if os.path.exists(self.path.GetLabelText()+"\\temp") == False:
+        # if os.path.exists(self.path.GetLabelText()+"\\temp") == False:
         #    os.makedirs(self.path.GetLabelText()+"\\temp")
 
         args = utility.parse_args()
-        args.image_dir=self.path.GetLabelText()
-        args.det_model_dir="./config/ch_det_mv3_db/"
-        args.rec_model_dir="./config/ch_rec_mv3_crnn/"
-        args.use_gpu=False
+        args.image_dir = self.path.GetLabelText()
+        args.det_model_dir = "./config/ch_det_mv3_db/"
+        args.rec_model_dir = "./config/ch_rec_mv3_crnn/"
+        args.use_gpu = False
         print("初始化系统")
+        config = configparser.ConfigParser()
+        config.read("./config/config.ini", encoding='UTF-8')
+        roadMapList = config.items('RoadWord')
         image_file_list = ocr_sys.get_image_file_list(args.image_dir)
         text_sys = ocr_sys.TextSystem(args)
         print("完成初始化")
-        imgFileDict ={}
+        imgFileDict = {}
         for image_file in image_file_list:
             imgFileDict[image_file] = image_file
 
         textNoDict = {}
         for n in range(len(image_file_list)):
             try:
-                self.tipCurtLabel.SetLabelText("当前第"+str(n+1)+"个")
+                self.tipCurtLabel.SetLabelText("当前第" + str(n + 1) + "个")
                 print(self.tipCurtLabel.GetLabelText())
                 image_file = image_file_list[n]
                 image_file = imgFileDict.get(image_file, "")
@@ -110,13 +118,19 @@ class MainFrame(wx.Frame):
                     text, score = rec_res[dno]
                     if score >= 0.5:
                         text_str = "%s, %.3f" % (text, score)
-                        print(text_str)
+                        # print(text_str)
                         tempText = self.removeNum(text)
+                        tempText2 = self.removeNum(finalText)
                         # finalTextDict[tempText] = finalText
-                        if len(tempText) > len(finalText):
+                        if len(tempText) > len(tempText2):
                             finalText = text
                 if finalText == "":
                     continue
+                for i in range(len(roadMapList)):
+                    roadWord = roadMapList[i][0]
+                    if finalText.find(roadWord) != -1:
+                        finalText = finalText.replace(roadWord, roadMapList[i][1])
+
                 # finalText = finalTextDict.get(finalText, "")
 
                 # 对出现的字符串进行出现次数编号
@@ -125,22 +139,31 @@ class MainFrame(wx.Frame):
                     textNoDict[finalText] = 1
                 else:
                     textNoDict[finalText] = (num + 1)
-                print("finalText:", finalText,"append num:"+str(num))
+                print("finalText:", finalText, "append num:" + str(num))
 
                 try:
                     if num == 0:
-                        finalFileName = filePath+"\\"+finalText+ext
+                        finalFileName = filePath + "\\" + finalText + ext
                         if finalFileName == image_file:
                             continue
                         file1 = Path(finalFileName)
                         if file1.exists():
-                            imgFileDict[finalFileName] = filePath+"\\"+finalText+self.getRandom(4)+ext
-                        os.rename(image_file, filePath+"\\"+finalText+ext)
+                            newName = filePath + "\\" + finalText + self.getRandom(4) + ext
+                            imgFileDict[finalFileName] = newName
+                            os.rename(finalFileName, newName)
+                        else:
+                            os.rename(image_file, filePath + "\\" + finalText + ext)
                     else:
-                        finalFileName = filePath+"\\"+finalText+str(num)+ext
+                        finalFileName = filePath + "\\" + finalText + str(num) + ext
                         if finalFileName == image_file:
                             continue
-                        os.rename(image_file, filePath+"\\"+finalText+str(num)+ext)
+                        file1 = Path(finalFileName)
+                        if file1.exists():
+                            newName = filePath + "\\" + finalText + self.getRandom(4) + ext
+                            imgFileDict[finalFileName] = newName
+                            os.rename(finalFileName, newName)
+                        else:
+                            os.rename(image_file, filePath + "\\" + finalText + str(num) + ext)
                 except Exception as e:
                     print(e)
                     # os.rename(image_file, filePath+"/"+finalNo+"(标识码"+str(n)+")"+ext)
@@ -152,7 +175,7 @@ class MainFrame(wx.Frame):
                 #    if os.path.isfile(filepath):
                 #      os.remove(filepath)
                 #      print(str(filepath)+" removed!")
-                if len(image_file_list) == n+1:
+                if len(image_file_list) == n + 1:
                     self.tipCurtLabel.SetLabelText("处理完成")
                 print("处理完成")
             except Exception as e:
@@ -161,23 +184,20 @@ class MainFrame(wx.Frame):
         self.selectBtn.Enable()
         self.startBtn.Enable()
 
-
-
     ## 读取图像，解决imread不能读取中文路径的问题
-    def cv_imread(self,filePath):
-        cv_img=cv2.imdecode(np.fromfile(filePath,dtype=np.uint8),-1)
+    def cv_imread(self, filePath):
+        cv_img = cv2.imdecode(np.fromfile(filePath, dtype=np.uint8), -1)
         ## imdecode读取的是rgb，如果后续需要opencv处理的话，需要转换成bgr，转换后图片颜色会变化
         ##cv_img=cv2.cvtColor(cv_img,cv2.COLOR_RGB2BGR)
-        #cv_img=Image.open(filePath).convert('RGBA')
+        # cv_img=Image.open(filePath).convert('RGBA')
         return cv_img
 
-
-    def removeNum(self,a):
-        b=[]
+    def removeNum(self, a):
+        b = []
         for i in a:
             if i not in "0123456789":
                 b.append(i)
-        return("".join(b))
+        return ("".join(b))
 
     def getRandom(self, n):
         s = ""
--- BatchOcr2.py
+++ BatchOcr2.py
@@ -5,15 +5,15 @@ import os
 import cv2
 import numpy as np
 import threading
-import tools.infer.utility as utility
-import tools.infer.predict_system as ocr_sys
 import time
 from pathlib import Path
 from pyzbar import pyzbar
-# from PIL import Image
+import tools.infer.utility as utility
+import tools.infer.predict_system as ocr_sys
 
 APP_TITLE = u'识别改名'
 APP_ICON = 'res/python.ico'
+
 
 # '道路识别'
 
@@ -25,14 +25,16 @@ class MainFrame(wx.Frame):
         self.SetSize((400, 160))
         self.Center()
 
-        self.pathLabel = wx.StaticText(self, -1, u'文件路径：', pos=(10, 20), size=(60, -1), name='pathLabel', style=wx.TE_LEFT)
+        self.pathLabel = wx.StaticText(self, -1, u'文件路径：', pos=(10, 20), size=(60, -1), name='pathLabel',
+                                       style=wx.TE_LEFT)
         self.path = wx.StaticText(self, -1, u'未选择', pos=(80, 20), size=(200, -1), name='path', style=wx.TE_LEFT)
 
         self.selectBtn = wx.Button(self, -1, u'选择', pos=(10, 50), size=(60, -1), style=wx.ALIGN_LEFT)
         self.startBtn = wx.Button(self, -1, u'开始', pos=(100, 50), size=(60, -1), style=wx.ALIGN_LEFT)
 
         self.tipLabel = wx.StaticText(self, -1, u'', pos=(10, 90), size=(200, -1), name='tipLabel', style=wx.TE_LEFT)
-        self.tipCurtLabel = wx.StaticText(self, -1, u'', pos=(80, 90), size=(200, -1), name='tipCurtLabel', style=wx.TE_LEFT)
+        self.tipCurtLabel = wx.StaticText(self, -1, u'', pos=(80, 90), size=(200, -1), name='tipCurtLabel',
+                                          style=wx.TE_LEFT)
 
         self.Bind(wx.EVT_BUTTON, self.OnSelect, self.selectBtn)
         self.Bind(wx.EVT_BUTTON, self.OnStart, self.startBtn)
@@ -42,12 +44,12 @@ class MainFrame(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             print(dlg.GetPath())  # 文件夹路径
             self.path.SetLabelText(dlg.GetPath())
-            self.tipLabel.SetLabelText("总共:"+str(len(os.listdir(self.path.GetLabelText()))))
+            self.tipLabel.SetLabelText("总共:" + str(len(os.listdir(self.path.GetLabelText()))))
         dlg.Destroy()
 
     def OnStart(self, event):
         if self.path.GetLabelText() == "未选择":
-            msgDialog = wx.MessageDialog(parent=None, message = u"选择文件夹",style = wx.OK)
+            msgDialog = wx.MessageDialog(parent=None, message=u"选择文件夹", style=wx.OK)
             if msgDialog.ShowModal() == wx.ID_OK:
                 print(1)
             return
@@ -61,23 +63,23 @@ class MainFrame(wx.Frame):
     def OnStart2(self):
         print(self.path.GetLabelText())
 
-        # args = utility.parse_args()
-        # args.image_dir=self.path.GetLabelText()
-        # args.det_model_dir="./config/ch_det_mv3_db/"
-        # args.rec_model_dir="./config/ch_rec_mv3_crnn/"
-        # args.use_gpu=False
+        args = utility.parse_args()
+        args.image_dir = self.path.GetLabelText()
+        args.det_model_dir = "./config/ch_det_mv3_db/"
+        args.rec_model_dir = "./config/ch_rec_mv3_crnn/"
+        args.use_gpu = False
         print("初始化系统")
         image_file_list = self.get_image_file_list(self.path.GetLabelText())
-        # text_sys = ocr_sys.TextSystem(args)
+        text_sys = ocr_sys.TextSystem(args)
         print("完成初始化")
-        imgFileDict ={}
+        imgFileDict = {}
         for image_file in image_file_list:
             imgFileDict[image_file] = image_file
 
         textNoDict = {}
         for n in range(len(image_file_list)):
             try:
-                self.tipCurtLabel.SetLabelText("当前第"+str(n+1)+"个")
+                self.tipCurtLabel.SetLabelText("当前第" + str(n + 1) + "个")
                 print(self.tipCurtLabel.GetLabelText())
                 image_file = image_file_list[n]
                 image_file = imgFileDict.get(image_file, "")
@@ -90,34 +92,35 @@ class MainFrame(wx.Frame):
                 if img is None:
                     print("error in loading image:{}".format(image_file))
                     continue
-                img = self.cropImg(img)
-                cv2.imshow('img', img)
-                cv2.waitKey()
+                img1 = self.cropImg(img)
+                # cv2.imshow('img', img)
+                # cv2.waitKey()
                 startTime = time.time()
-                txt_list = pyzbar.decode(img)
+                txt_list = pyzbar.decode(img1)
                 if len(txt_list) == 0:
-                    img = cv2.resize(img, (0, 0), fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
-                    txt_list = pyzbar.decode(img)
-                print(txt_list)
-                # dt_boxes, rec_res = text_sys(img)
-                elapse = time.time() - startTime
-                print("Predict time of %s: %.3fs" % (image_file, elapse))
-                dt_num = 0
+                    img1 = cv2.resize(img1, (0, 0), fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
+                    txt_list = pyzbar.decode(img1)
 
-                # finalTextDict = {}
-                finalText = ""
-                # for dno in range(dt_num):
-                #     text, score = rec_res[dno]
-                #     if score >= 0.5:
-                #         text_str = "%s, %.3f" % (text, score)
-                #         print(text_str)
-                #         tempText = self.removeNum(text)
-                #         # finalTextDict[tempText] = finalText
-                #         if len(tempText) > len(finalText):
-                #             finalText = text
+                if len(txt_list) == 0:
+                    continue
+
+                finalText = str(txt_list[0].data, encoding="utf-8")
+                print(finalText)
                 if finalText == "":
                     continue
-                # finalText = finalTextDict.get(finalText, "")
+
+                textArr = finalText.split(',')
+                if len(textArr) < 7:
+                    continue
+                finalText = textArr[5] + '-' + textArr[3] + '-' + textArr[6].split(".")[0]
+
+                # 对受理方式进行ocr识别 先裁切图片
+                img2 = self.cropImg2(img)
+                cv2.imshow('img', img2)
+                cv2.waitKey()
+                dt_boxes, rec_res = text_sys(img2)
+                elapse = time.time() - startTime
+                print("Predict time of %s: %.3fs" % (image_file, elapse))
 
                 # 对出现的字符串进行出现次数编号
                 num = textNoDict.get(finalText, 0)
@@ -125,26 +128,31 @@ class MainFrame(wx.Frame):
                     textNoDict[finalText] = 1
                 else:
                     textNoDict[finalText] = (num + 1)
-                print("finalText:", finalText,"append num:"+str(num))
+                print("finalText:", finalText, "append num:" + str(num))
 
+                num = 0
                 try:
                     if num == 0:
-                        finalFileName = filePath+"\\"+finalText+ext
+                        finalFileName = filePath + "\\" + finalText + ext
                         if finalFileName == image_file:
                             continue
                         file1 = Path(finalFileName)
                         if file1.exists():
-                            imgFileDict[finalFileName] = filePath+"\\"+finalText+self.getRandom(4)+ext
-                        os.rename(image_file, filePath+"\\"+finalText+ext)
+                            newName = filePath + "\\" + finalText + "(" + str(textNoDict[finalText]) + ")" + ext
+                            if image_file == newName:
+                                continue
+                            os.rename(image_file, newName)
+                        else:
+                            os.rename(image_file, filePath + "\\" + finalText + ext)
                     else:
-                        finalFileName = filePath+"\\"+finalText+str(num)+ext
+                        finalFileName = filePath + "\\" + finalText + "(" + str(num) + ")" + ext
                         if finalFileName == image_file:
                             continue
-                        os.rename(image_file, filePath+"\\"+finalText+str(num)+ext)
+                        os.rename(image_file, filePath + "\\" + finalText + "(" + str(num) + ")" + ext)
                 except Exception as e:
                     print(e)
 
-                if len(image_file_list) == n+1:
+                if len(image_file_list) == n + 1:
                     self.tipCurtLabel.SetLabelText("处理完成")
                 print("处理完成")
             except Exception as e:
@@ -153,23 +161,20 @@ class MainFrame(wx.Frame):
         self.selectBtn.Enable()
         self.startBtn.Enable()
 
-
-
     ## 读取图像，解决imread不能读取中文路径的问题
-    def cv_imread(self,filePath):
-        cv_img=cv2.imdecode(np.fromfile(filePath,dtype=np.uint8),-1)
+    def cv_imread(self, filePath):
+        cv_img = cv2.imdecode(np.fromfile(filePath, dtype=np.uint8), -1)
         ## imdecode读取的是rgb，如果后续需要opencv处理的话，需要转换成bgr，转换后图片颜色会变化
         ##cv_img=cv2.cvtColor(cv_img,cv2.COLOR_RGB2BGR)
-        #cv_img=Image.open(filePath).convert('RGBA')
+        # cv_img=Image.open(filePath).convert('RGBA')
         return cv_img
 
-
-    def removeNum(self,a):
-        b=[]
+    def removeNum(self, a):
+        b = []
         for i in a:
             if i not in "0123456789":
                 b.append(i)
-        return("".join(b))
+        return ("".join(b))
 
     def getRandom(self, n):
         s = ""
@@ -201,45 +206,54 @@ class MainFrame(wx.Frame):
         hs = int(height * 0.25)
         cropped_img = img[0:hs, ws:width]
 
+        # 裁剪出二维码区域
         gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
 
-        gradX = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 1, dy = 0, ksize = -1)
-        gradY = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 0, dy = 1, ksize = -1)
+        gradX = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+        gradY = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
 
         gradient = cv2.subtract(gradX, gradY)
         gradient = cv2.convertScaleAbs(gradient)
 
-        #cv2.imshow("gradient",gradient)
-        #原本没有过滤颜色通道的时候，这个高斯模糊有效，但是如果进行了颜色过滤，不用高斯模糊效果更好
-        #blurred = cv2.blur(gradient, (9, 9))
+        # cv2.imshow("gradient",gradient)
+        # 原本没有过滤颜色通道的时候，这个高斯模糊有效，但是如果进行了颜色过滤，不用高斯模糊效果更好
+        # blurred = cv2.blur(gradient, (9, 9))
         (_, thresh) = cv2.threshold(gradient, 225, 255, cv2.THRESH_BINARY)
-        #cv2.imshow("thresh",thresh)
-        #cv2.imwrite('thresh.jpg',thresh)
+        # cv2.imshow("thresh",thresh)
+        # cv2.imwrite('thresh.jpg',thresh)
         qx, qy = cropped_img.shape[0:2]
         qrFactor = 15
-        if qx * qy > 800 * 800 :
+        if qx * qy > 800 * 800:
             qrFactor = 21
-        if qx * qy < 500 * 500 :
+        if qx * qy < 500 * 500:
             qrFactor = 11
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (qrFactor, qrFactor))
         closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-        #cv2.imshow("closed",closed)
-        #cv2.imwrite('closed.jpg',closed)
+        # cv2.imshow("closed",closed)
+        # cv2.imwrite('closed.jpg',closed)
 
-        closed = cv2.erode(closed, None, iterations = 4)
-        closed = cv2.dilate(closed, None, iterations = 4)
-        #cv2.imwrite('closed1.jpg',closed)
-        cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        closed = cv2.erode(closed, None, iterations=4)
+        closed = cv2.dilate(closed, None, iterations=4)
+        # cv2.imwrite('closed1.jpg',closed)
+        cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
         if len(cnts) == 0:
             return cropped_img
         x, y, w, h = cv2.boundingRect(c)
-        cropped = cropped_img[y:y+h, x:x+w]
+        cropped = cropped_img[y:y + h, x:x + w]
         # draw_img = cv2.drawContours(cropped_img.copy(), c, -1, (0, 0, 255), 3)
         # cv2.imshow("draw_img", draw_img)
         # cv2.waitKey()
         return cropped
 
+    def cropImg2(self, img):
+        sp = img.shape
+        height = sp[0]
+        width = sp[1]
+        ws = int(width * 0.7)
+        hs = int(height * 0.5)
+        cropped_img = img[hs:height, int(0.2*width):ws]
+        return cropped_img
 
 class MainApp(wx.App):
     def OnInit(self):
